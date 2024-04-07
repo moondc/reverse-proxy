f9c5df05b46b927fa2d963e06f309b4e1a38d94f
Daniel Moon <daniel_moon16@yahoo.com>
2024-04-07 19:16:03 -0400

    simplifying build script, minor caveats though

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -12,40 +12,27 @@ envsubst "$SUBSTITUTION_VALUES" < nginx.conf.template > nginx.conf
 # Set script vars
 DOCKER_TAG="reverse-proxy"
 
-# Build docker image
-echo "Building target for arm64"
+echo "Switching builder"
 docker buildx use mybuilder
+
+
+echo "Building target for arm64"
 docker buildx build --platform linux/arm64 -t $DOCKER_TAG . --load
 
-echo "Build finished"
+echo "Stopping old container"
+ssh "$PI_USER@$PI_IP" "docker stop $DOCKER_TAG " || true
 
-# # Stop old containers
-# echo "Stopping all old containers"
-# OLD_IMAGES=$(ssh "$PI_USER@$PI_IP" 'docker images --filter "reference=$DOCKER_TAG*" --format "{{.ID}}"')
-# for OLD_IMAGE in $OLD_IMAGES; do
-#     CONTAINER_IDS=$(ssh "$PI_USER@$PI_IP" "docker ps -q --filter ancestor='$OLD_IMAGE'")
-#     for CONTAINER_ID in $CONTAINER_IDS; do
-#         ssh "$PI_USER@$PI_IP" "docker stop '$CONTAINER_ID'"
-#     done
-# done
-# echo "Done stopping old containers"
+echo "Removing old container"
+ssh "$PI_USER@$PI_IP" "docker container rm $DOCKER_TAG " || true
 
-# # Prune old containers and images
-# echo "Pruning all stopped containers and deleting unused images"
-# DELETED_ITEMS=$(ssh "$PI_USER@$PI_IP" "docker system prune -a -f")
-# echo $DELETED_ITEMS
-# echo "Done pruning"
+# echo "Removing old image"
+# ssh "$PI_USER@$PI_IP" "docker image rm $DOCKER_TAG " || true
 
-# Push image to remote server
-echo "Pushing to remote"
+echo "Pushing new image"
 docker save $DOCKER_TAG | bzip2 | ssh -l $PI_USER $PI_IP docker load
-echo "Done pushing"
 
-
-# Start container with auto restart
 echo "Starting Container"
-STARTED_CONTAINER=$(ssh "$PI_USER@$PI_IP" "docker run -d --network host -v /var/log/letsencrypt:/var/log/letsencrypt -v /etc/letsencrypt:/etc/letencrypt -v $(pwd)/nginx-logs:/var/log/nginx --restart unless-stopped \"$DOCKER_TAG\"")
-echo "Started container:$STARTED_CONTAINER"
+ssh "$PI_USER@$PI_IP" "docker run -d --network host -v /var/log/letsencrypt:/var/log/letsencrypt -v /etc/letsencrypt:/etc/letencrypt -v $(pwd)/nginx-logs:/var/log/nginx --restart unless-stopped --name $DOCKER_TAG \"$DOCKER_TAG\""
 
 # Restore builder
 docker buildx use default
